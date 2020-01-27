7526575e4c95e17c3e451d0d1da5795213d157ad
sunain1 <610331662@qq.com>
2020-01-27 18:30:53 +0800

    修改测试数据

--- src/qsort/test.java
+++ src/qsort/test.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class test {
 	public static void main(String[] args) throws IOException{
-		int i = 1, j =2;
+		int i = 3, j =2;
 		int k = i++ + i + ++j;
 		System.out.println(k);
 	}
